c63966e94c66abe6fa4058f7540307d07b6a1983
peterbu1 <peterfire3656@gmail.com>
2023-06-22 16:42:07 -0400

    Fixed single quotes escape

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -27,7 +27,7 @@ function Contact({ }: Props) {
       <div className='flex flex-col space-y-10'>
         <h4 className='text-4xl font-semibold text-center'>
            {" "}
-          <span className='decoration-[#f7ab0a]/50 underline'>Let's Connect.</span>
+          <span className='decoration-[#f7ab0a]/50 underline'>Let&apos;s Connect.</span>
         </h4>
         <div className='space-y-5'>
           <div>
