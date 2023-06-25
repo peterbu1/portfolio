f39dcccf5afc5b02e6f995d2a3ecb175d53268db
peterbu1 <peterfire3656@gmail.com>
2023-06-25 11:40:20 -0400

    added icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="shortcut icon" href='/peterb.png' />
       <Head />
       <body>
         <Main />
